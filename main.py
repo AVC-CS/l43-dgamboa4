d4510c366bbaf7e39fddc1730a5f1c7fc71570e5
Destiny Gamboa <dgamboa4@students.avc.edu>
2024-10-06 18:42:48 -0700

    done

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ def main():
     ########################################
     """
     total = 0
+    i = 0 
+    while (i < 5):
+        user_num = int(input('Enter your num '))
+        total += user_num
+        i += 1
+    print("Total: ", total)
 
     ########################################
     # Do not delete the return statement
